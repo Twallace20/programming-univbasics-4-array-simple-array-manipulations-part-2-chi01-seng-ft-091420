1776a4b60b435f0bd29b493ead9e1d808ce7c395
Tyler Wallace <tywallace2017@gmail.com>
2020-08-28 17:01:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,10 +14,10 @@ def using_flatten(instruments)
   flat_array = instruments.flatten 
 end
 
-def usind_delete(instructors, steven)
-  no_offense_steven = instructors.delete
+def usind_delete(instructors, "Steven")
+  no_offense_steven = instructors.delete["Steven"]
 end
 
-def using_delete_at(famous_robots)
-  deleted_robot = famous_robots.delete_at
+def using_delete_at(famous_robots, 2)
+  deleted_robot = famous_robots.delete_at[2]
 end
