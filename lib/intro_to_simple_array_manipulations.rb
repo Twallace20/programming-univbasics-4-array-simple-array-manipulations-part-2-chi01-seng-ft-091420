369122566a41db9434e8a0fe3830634bacc08e38
Tyler Wallace <tywallace2017@gmail.com>
2020-08-28 17:31:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,8 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_programming_languages , another_language)
-  new_array = list_of_programming_languages.insert(another_language)
+  t = list_of_programming_languages
+  new_array = t.insert(another_language)
 end 
 
 def using_uniq(haircuts)
