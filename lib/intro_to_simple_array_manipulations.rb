b66a74aa1e7e47b135980e14044c122e1456e56f
Tyler Wallace <tywallace2017@gmail.com>
2020-08-28 17:37:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,7 @@ def using_insert(list_of_programming_languages , another_language)
   t = list_of_programming_languages.length
   d = another_language.length
   new_array = list_of_programming_languages.insert(another_language)
+  new_arrays = (t) + (d)
 end 
 
 def using_uniq(haircuts)
