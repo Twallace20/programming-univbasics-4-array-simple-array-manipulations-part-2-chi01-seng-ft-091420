c4c9579db4fad1da6d8893ac547680c4e64898a6
Tyler Wallace <tywallace2017@gmail.com>
2020-08-28 18:06:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,8 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(array, element)
-  new_array = element.insert(array)
+  4.times do |element|
+  puts array
 end 
 
 def using_uniq(haircuts)
